79c9b01b91d942c8c8782714f1cc4ac50a209912
Yashaswi Kumar Mishra <mishrayashaswikumar@gmail.com>
2024-07-31 19:01:40 +0530

    dashboard

---
+++ src/components/FriendRequests.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { FC } from 'react'
+
+interface FriendRequestsProps {
+  
+}
+
+const FriendRequests: FC<FriendRequestsProps> = ({}) => {
+  return <div>FriendRequests</div>
+}
+
+export default FriendRequests
